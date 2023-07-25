88a17842a5414e12b2bd5e97da1f322b5746d475
Luca Bulles <lucabulles@gmail.com>
2023-07-25 16:28:38 +0200

    configured api to allow api requests from my angular frontend project that runs on 4200

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -4,9 +4,19 @@ global using SuperHeroAPI.Data;
 using SuperHeroAPI.Services.SuperHeroService;
 using SuperHeroAPI.Repositories.SuperHeroRepository;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200");
+                      });
+});
 
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,6 +39,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
