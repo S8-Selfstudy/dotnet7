60d8cb149e7f266981b24d43f9a7d052fec39ec9
Luca Bulles <lucabulles@gmail.com>
2023-07-26 16:07:12 +0200

    updated cors policy

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -4,16 +4,15 @@ global using SuperHeroAPI.Data;
 using SuperHeroAPI.Services.SuperHeroService;
 using SuperHeroAPI.Repositories.SuperHeroRepository;
 
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors(options =>
+
+const string corsPolicy = "cors-app-policy";
+builder.Services.AddCors(c => c.AddPolicy(corsPolicy, corsPolicyBuilder =>
 {
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("http://localhost:4200");
-                      });
-});
+    corsPolicyBuilder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
 
 // Add services to the container.
 
@@ -41,7 +40,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.UseCors(MyAllowSpecificOrigins);
+app.UseCors(corsPolicy);
 
 app.UseAuthorization();
 
